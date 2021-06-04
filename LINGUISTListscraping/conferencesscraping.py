8b804cc332943c69a6df9e2a9d67bf394c1453e6
AlidaSchefers <alidadschefers@gmail.com>
2021-06-04 17:47:14 -0400

    skips sessions listed in parentheses and writes all conference postings into the csv file

--- LINGUISTListscraping/conferencesscraping.py
+++ LINGUISTListscraping/conferencesscraping.py
@@ -5,15 +5,16 @@ import re #for regular expressions
 
 #this program currently scrapes from the HTML file of LINGUISTList's "browse current conferences" page.
 
-#the csv this program writes...
-    #contains each conference's: title, location, dates, and LINGUISTList url
-    #does not contain each conference's: Linguistic Field(s), Subject Language(s), website
-#this version currently:
-    #skips posts that are tabbed and labeled as "session"
-    #skips calls for papers
+#this program writes a csv that...
+    #contains each conference's title, location, dates, and LINGUISTList url
+    #does not contain each conference's Linguistic Field(s), Subject Language(s), and website
+#this version currently skips:
+    #posts that are tabbed and labeled as "session"
+    #posts that are labeled as "session" in the conference title parentheses.
+    #calls for papers
     #skips mid-page date dividers
 
-#using the HTML file:
+#scraping a HTML file of the website page:
 with open('browse_current_conferences.html') as html_file:
     soup = BeautifulSoup(html_file, features="lxml")
     csv_file = open('conferences_scrape.csv', 'w')
@@ -24,27 +25,44 @@ with open('browse_current_conferences.html') as html_file:
 
     listing = [item for item in listing if not (item.find('td', {'align':'left', 'valign':'top'}))] #removes tabbed session postings + browse-by-date line on top of page
     listing = [item for item in listing if not (item.find('span', class_='important'))] #removes call for papers and date dividers
-    #UNSUCCESSFUL: # listing_wo_para_sessions = [item for item in listing_wo_tabbed_sessions if not (item.find('a', class_='important'))] #removes sessions that are only written in parantheses. 
+    listing = [item for item in listing if not ('Session' in item.find('td').text)] #removes sessions that are only labeled as sessions in parentheses.
 
-    for posting in listing[0:10]: #test with a few conferences (no session posts)
-        title_location_and_dates_text = posting.find('td').text 
-            #e.g. 11th Conference of the International Gender and Language Association (IGALA11)&nbsp;[London (Online)] [22-Jun-2021 - 24-Jun-2021]
-        conference_id = posting.find('a')['href'][-6:]
-        dates = re.findall('\[[^\]]*\]', title_location_and_dates_text)[1][1:-1] #[1:-1] removes the brackets
-        
+    for posting in listing[0:]: #test with a few conferences (no session posts)
+        # print(posting)
+
+        #title
         title = posting.find('a').text[0:-1] #[0:-1] removes the \n at the end
-        location = re.findall('\[[^\]]*\]', title_location_and_dates_text)[0][1:-1]
-        start_date = dates[0:11]
-        end_date = dates[14:len(dates)]
+
+        #location, start_date, end_date
+        try:
+            title_location_and_dates_text = posting.find('td').text 
+                #e.g. 11th Conference of the International Gender and Language Association (IGALA11)&nbsp;[London (Online)] [22-Jun-2021 - 24-Jun-2021]
+            location = re.findall('\[[^\]]*\]', title_location_and_dates_text)[0][1:-1]
+            dates = re.findall('\[[^\]]*\]', title_location_and_dates_text)[1][1:-1] #[1:-1] removes the brackets      
+            start_date = dates[0:11]
+            end_date = dates[14:len(dates)]
+        except Exception as e:
+            location = None;
+            start_date = None;
+            end_date = None;
+
+        #linguistlist_url
+        conference_id = posting.find('a')['href'][-6:]
         linguistlist_url = f'https://old.linguistlist.org/callconf/browse-conf-action.cfm?ConfID={conference_id}'
 
+        #write row with the conference's details
         csv_writer.writerow([title, location, start_date, end_date, linguistlist_url])
 
 csv_file.close()
 
-#Things to skip:
+#scraping from the website page:
+    # source = requests.get('https://old.linguistlist.org/callconf/browse-current.cfm?type=Conf').text
+    # soup = BeautifulSoup(source, 'lxml')
+
+# Initial notes -------------------------------
+    #Things to skip:
     #DONE: the sessions indented. inside tr, then inside td use the align="left" valign="top attributes to find sessions
-    #NOT YET: sessions described in parentheses: --> can't do yet. so we'll keep and have blank dates/etc and take the first link in href.
+    #DONE: sessions described in parentheses: --> can't do yet. so we'll keep and have blank dates/etc and take the first link in href.
         # <tr>
         # <td colspan="2" align="left"><a href="/callconf/browse-conf-action.cfm?ConfID=426236">Accountability of Discursive Action and the Private-Public Interface
         # </a>
@@ -60,7 +78,3 @@ csv_file.close()
         # </a>
         # &nbsp;[Online] [17-Jul-2021 - 18-Jul-2021]<br /></td>
         # </tr>
-
-#using the website:
-    # source = requests.get('https://old.linguistlist.org/callconf/browse-current.cfm?type=Conf').text
-    # soup = BeautifulSoup(source, 'lxml')
